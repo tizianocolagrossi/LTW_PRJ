5ab4eca3f31153359d230a84a4c558f513abf080
root <root@localhost.localdomain>
2018-11-14 16:51:20 +0100

    modifica logic.js

--- js/logic.js
+++ js/logic.js
@@ -1,15 +1,28 @@
-var ID_reg_expression = /[A-Z][A-Z]\d{7}/;
-var regexpID = RegExp('[A-Z][A-Z]\d{7}');
+var ID_reg_expression = /[A-Z][A-Z]\d{7} || C[A-Z]\d{5}[A-Z][A-Z]/;
 
-function validateID(){
-	var text_value = document.getElementByName("name").value;
-	var ret_boolean = regexpID.test(text_value);
-	console.log(ret_boolean);
-	return ret_boolean;
+function inizializeDB(){
+	var file = new File([txt], "database.txt", {type: "application/octet-stream"});
+	//var blobUrl = (URL || webkitURL).createObjectURL(file);
 }
 
-function validatePW(ID_code){
-	//dato il codice della carta d'identità (precedentemente controllato), controllare da file se la password è valida
+function validateID(ID_value){
+	var ret_boolean = ID_reg_expression.test(ID_value);
+	if(ret_boolean) validatePW(ID_value);
+	else error("Codice non valido");
 }
 
+function validatePW(ID_value){
+	error("password");
+	//dato il codice della carta d'identità (precedentemente controllato), controllare da file se la password è associata a quell'ID
+}
 
+function register(){
+	var ID_value = document.getElementByName("ID").value;
+	if(validateID(ID_value)){
+		
+	}
+}
+
+function error(error_message){
+	window.alert(error_message)
+}
