c56925ab76c661f2d432710dba721a26b05b0761
davidedc97 <davidedc1997@gmail.com>
2018-11-26 14:07:13 +0100

    Changes to be committed: 	modified:   js/cal.js

--- js/cal.js
+++ js/cal.js
@@ -32,6 +32,52 @@ function buildToday(){
 	campo_oggi.innerHTML = oggi;
 }
 
+function getLastDay(month){
+	switch(month+1){
+		case (1||3||5||7||8||10||12):
+			return 31;
+		case 2:
+			return 28;
+		case (11||4||6||9):
+			return 30;
+	}
+}
+
+function buildDays(year, month){
+	var objDate =  new Date(year, month, 2);
+	var first_day = objDate.getDay() + 1;
+	var last_day = getLastDay(month);
+	var active_month = false; 
+	var counter = (new Date(year, month, 2 - (first_day-1)) ).getDate();
+	var giorno;
+	
+	for(i=1; i<7; i++){
+		for(j=1; j<8; j++){
+		
+			if(i==1 && j==first_day){
+				active_month = true;
+				counter = 1;
+			}
+			else if(i>=5 && counter==last_day+1){
+				active_month = false;
+				counter = 1;
+			}
+			
+			var index = (i+"_"+j).toString();
+			giorno = document.getElementById(index);
+			console.log(index);
+			console.log(giorno);
+			
+			/*
+			if(active_month) giorno.removeClass("no-mo");
+			else giorno.addClass("no-mo");
+			
+			giorno.innerText = counter;
+			*/
+		}	
+	}
+}
+
 function buildMonth(){
 	displayMonth(mese-1);
 }
@@ -40,6 +86,7 @@ function displayMonth(month){
 	var campo_mese = document.getElementById("mese");
 	campo_mese.innerHTML = monthText[month].toUpperCase();
 	display_mese = month + 1;
+	buildDays(2018, month);
 }
 
 function nextMonth(){
@@ -54,5 +101,6 @@ function prevMonth(){
 	if(indice < 0) indice = 11;
 	displayMonth(indice%12)
 }
+
 
 
